9cb453d89e3f94a41d4ccc387ec4f3af8f3a6aae
Geet Salame <geetsalame156@gmail.com>
2023-12-15 12:47:25 +0530

    some deploy changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <div id='footer'>
-            <p>KanbanBoard developed by <a href="https://www.linkedin.com/in/geet-salame-57a6a71b6/">Geet Salame</a> </p>
+            <p>KanbanBoard developed by <a href="https://www.linkedin.com/in/geet-salame-57a6a71b6/" target='_blank'>Geet Salame</a> </p>
         </div>
     )
 }
