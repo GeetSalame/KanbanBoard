7b9f30f37a1b9cecc1a92cf899893eedd30a6f3b
Geet Salame <geetsalame156@gmail.com>
2023-12-15 13:02:22 +0530

    handled wrong routing

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import notfound from '../img/pagenotfound.png';
+import { useNavigate } from 'react-router-dom';
 
 function PageNotFound() {
+  const navigate = useNavigate();
   return (
-    <div>PageNotFound</div>
+    <div id='pagenotfound'>
+      <h1>Page Not Found</h1>
+      <img src={notfound} alt="Page Not Found" />
+      <button className='btn1' onClick={()=>{navigate('/')}}>Go back to Home</button>
+    </div>
   )
 }
 
